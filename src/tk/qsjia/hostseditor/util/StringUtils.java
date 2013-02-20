9f7840321e3f6c2609c1da848b15c14c8ad974aa
QSJia <iiji86@gmail.com>
2013-02-20 16:51:51 +0800

    create hosts file; mount '/system' as rw/ro; some other changes

---
+++ src/tk/qsjia/hostseditor/util/StringUtils.java
@@ -0,0 +1,11 @@
+package tk.qsjia.hostseditor.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SJia
+ * Date: 13-1-9
+ * Time: 下午3:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringUtils {
+}
